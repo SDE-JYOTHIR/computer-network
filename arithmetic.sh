2a0391fb6a90b9884f5df0bc37d1bbb37f508d4e
MerinPJohn <117884884+MerinPJohn@users.noreply.github.com>
2023-04-05 12:02:52 +0530

    Add files via upload

---
+++ arithmetic.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+echo "Enter two integers:"
+read a b
+echo "Addition of and b:"
+echo $(( $a + $b ))
+echo "Substraction of a and b:"
+echo $(( $a - $b ))
+echo "Multiplication of a and b:"
+echo $(( $a * $b ))
+echo "Division of a and b:"
+echo $(( $a / $b )) 
